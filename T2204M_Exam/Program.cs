1e9c089fce3212226cb18c825e042c82d075c7d1
HungTranXD <hungtranxd2697@gmail.com>
2023-02-09 10:14:12 +0700

    Finished

--- T2204M_Exam/Program.cs
+++ T2204M_Exam/Program.cs
@@ -6,15 +6,15 @@ public class Program
     public static void Main(string[] args)
     {
         //Exercies 1: 
-        //Cylinder c = new Cylinder();
-        //Console.WriteLine("Enter the dimension of the cylinder");
-        //Console.Write("Radius: ");
-        //c.Radius = Convert.ToDouble(Console.ReadLine());
-        //Console.Write("Height: ");
-        //c.Height = Convert.ToDouble(Console.ReadLine());
+        Cylinder c = new Cylinder();
+        Console.WriteLine("Enter the dimension of the cylinder");
+        Console.Write("Radius: ");
+        c.Radius = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Height: ");
+        c.Height = Convert.ToDouble(Console.ReadLine());
 
-        //c.Process();
-        //c.Result();
+        c.Process();
+        c.Result();
 
         //Exercies 2:
         Lion l = new Lion();
